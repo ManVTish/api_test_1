c6536155dfaa16c3cd2874d08870837a55edf36c
Manish Roy <manishroy0112@gmail.com>
2022-04-10 17:56:48 +0530

    author attribute in book was removed and update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_121810) do
+ActiveRecord::Schema.define(version: 2022_04_10_122534) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2022_04_10_121810) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
