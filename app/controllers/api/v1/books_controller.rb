e83b3521de6d159c17139d747b3ddab244b1fd42
Manish Roy <manishroy0112@gmail.com>
2022-04-10 18:30:23 +0530

    BooksRepresenter used in index action

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -5,7 +5,8 @@ module Api
     class BooksController < ApplicationController
 
       def index
-        render json: Book.all
+        books = Book.all
+        render json: BooksRepresenter.new(books).as_json
       end
 
       def create
