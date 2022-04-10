ca793c50e8c2095c2b88ee09673b6d2f92e4f737
Manish Roy <manishroy0112@gmail.com>
2022-04-10 11:52:34 +0530

    create controller was created

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,14 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    book = Book.new(params[:title], params[:author])
+
+    if book.save
+      render json: book, status: :created
+    else
+      render status: :unprocessable_entity
+    end
+  end
 end
