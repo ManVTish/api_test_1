97482dc68d38db4ac2999eca5ebac465bca1850f
Manish Roy <manishroy0112@gmail.com>
2022-04-10 15:48:50 +0530

    private method for params used

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(title: params[:title], author: params[:author])
+    #book = Book.new(title: params[:title], author: params[:author])
+    book = Book.new(book_params)
 
     if book.save
       render json: book, status: :created
     else
       render json: book.errors, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
   end
 end
