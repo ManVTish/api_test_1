ef6a99d4574eb0f64292c55f793aa8d29747eec8
Manish Roy <manishroy0112@gmail.com>
2022-04-11 21:02:15 +0530

    create method populated

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,7 +4,10 @@ module Api
   
       class AuthenticationController < ApplicationController
         def create
-          
+          p params.require(:username).inspect
+          p params.require(:username).inspect
+
+          render json: {token: '123'}, status: unprocessable_entity
         end
       end
     end
