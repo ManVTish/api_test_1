0ac5c67fb2d1d46010d5c14ae5c494347342205d
Manish Roy <manishroy0112@gmail.com>
2022-04-11 21:09:06 +0530

    ParameterMissing rescue added in authentication controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -3,11 +3,18 @@ module Api
     module V1
   
       class AuthenticationController < ApplicationController
+        rescue_from ActionController::ParameterMissing, with: :missing_parameter
         def create
           p params.require(:username).inspect
           p params.require(:username).inspect
 
-          render json: {token: '123'}, status: unprocessable_entity
+          render json: {token: '123'}, status: :unprocessable_entity
+        end
+
+        private
+
+        def missing_parameter(e)
+          render json: {error: e.message }, status: :unprocessable_entity
         end
       end
     end
