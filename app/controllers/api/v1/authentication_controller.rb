892081178cdbe854cf51d4dbc46965582cd2c798
Manish Roy <manishroy0112@gmail.com>
2022-04-12 13:29:11 +0530

    user method in private and removed from create

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -10,7 +10,6 @@ module Api
         rescue_from AuthenticationError, with: :handle_unauthenticated
 
         def create
-          user = User.find_by(username: params.require(:username))
           #p params.require(:username).inspect
           raise AuthenticationError unless user.authenticate(params.require(:password))
           token = AuthenticationTokenService.call(user.id)
@@ -19,6 +18,10 @@ module Api
         end
 
         private
+
+        def user
+          @user ||= User.find_by(username: params.require(:username))
+        end
 
         def missing_parameter(e)
           render json: {error: e.message }, status: :unprocessable_entity
