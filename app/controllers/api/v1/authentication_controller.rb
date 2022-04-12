eba83a35bb6dd82f36fe066df9c95e2b2e60e2a8
Manish Roy <manishroy0112@gmail.com>
2022-04-12 13:24:59 +0530

    rescue_from AuthenticationError added in Authentication controller

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,7 +5,10 @@ module Api
       class AuthenticationController < ApplicationController
         class AuthenticationError < StandardError
         end
+
         rescue_from ActionController::ParameterMissing, with: :missing_parameter
+        rescue_from AuthenticationError, with: :handle_unauthenticated
+
         def create
           user = User.find_by(username: params.require(:username))
           #p params.require(:username).inspect
@@ -19,6 +22,10 @@ module Api
 
         def missing_parameter(e)
           render json: {error: e.message }, status: :unprocessable_entity
+        end
+
+        def handle_unauthenticated
+          header :unauthorized
         end
       end
     end
