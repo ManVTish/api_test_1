8e4a9018f54a9f95962ed4dbc87f7ece2ae48724
Manish Roy <manishroy0112@gmail.com>
2022-04-12 14:28:50 +0530

    token decode method added in Authentication Token Service class

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -7,4 +7,9 @@ class AuthenticationTokenService
     
     token = JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
   end
+
+  def self.decode(token)
+    decoded_token = JWT.decode token, HMAC_SECRET, true, {algorithm: ALGORITHM_TYPE}
+    decoded_token[0]['data']
+  end
 end
