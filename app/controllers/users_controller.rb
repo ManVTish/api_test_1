f0c40eed1258aef84f9056a34fa2634bef3e2963
Manish Roy <manishroy0112@gmail.com>
2022-04-23 23:11:57 +0530

    user_params method added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def create
     user = User.new(user_params)
+
+    if user.save
+      render json: :user, status: :created
+    end
   end
 
   def destroy
