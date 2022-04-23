2ef9ad44ed7b9d11d4eca9942eadabc86105c597
Manish Roy <manishroy0112@gmail.com>
2022-04-23 22:54:32 +0530

    create and destroy method created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :books, only: [:index, :create, :destroy]
+      resources :users, only: [:create, :destroy]
       
       post 'authenticate', to: 'authentication#create'
     end
