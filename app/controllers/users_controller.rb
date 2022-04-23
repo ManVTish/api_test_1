d44c49019b0e379b50a76fd64c5c0da4cc89b68d
Manish Roy <manishroy0112@gmail.com>
2022-04-23 23:22:33 +0530

    user_params method updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:users).permit(:username, :password)
+    params.require(:user).permit(:username, :password)
   end
 end
